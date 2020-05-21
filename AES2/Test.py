a05f09bdc349d686698c1b3e686afa437df31d4d
leonw <leon.weinmann@gmx.de>
2020-05-21 23:33:01 +0200

    i

--- AES2/Test.py
+++ AES2/Test.py
@@ -1,10 +1,2 @@
-file_name = "12334"
+s = "wasd"
 
-try:
-    try:
-        file = open(file_name, "rb")
-        data = file.read()
-    except NameError:
-        print("File not Found")
-finally:
-    file.close()
--- AES2/main.py
+++ AES2/main.py
@@ -1,52 +1,76 @@
 import base64
+import string
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
-from secrets import token_bytes
+from secrets import token_bytes, choice
+from os import path
 
 
 # backend = default_backend()
 
 class Encrypt:
 
-    pepper = 0
-
     def main(self):
-        key, salt = self.generate_key(b"Hello")
-        self.encrypt_file("test.txt", key)
-        input("> ")
-        self.decrypt("test.txt", key)
+        key, salt = self.generate_key()
+        self.encrypt_file("test.txt", key, salt)
 
-    def generate_key(self, user_password):
+    def generate_key(self):
+        print("Enter password")
+        user_password = input("> ")
+        # Pepper the user password
+        user_password += choice(string.ascii_letters + string.digits)
         salt = token_bytes(16)
-        kdf = PBKDF2HMAC(algorithm=hashes.SHA3_256(), length=32, salt=salt, iterations=100000, backend=default_backend())
-        key = base64.urlsafe_b64encode(kdf.derive(user_password))
+        kdf = PBKDF2HMAC(algorithm=hashes.SHA3_256(), length=32, salt=salt, iterations=111355, backend=default_backend())
+        key = base64.urlsafe_b64encode(kdf.derive(user_password.encode()))
         return key, salt
 
+    def check_key(self, salt):
+        print("Enter password")
+        user_password = input("> ")
+        kdf = PBKDF2HMAC(algorithm=hashes.SHA3_256(), length=32, salt=salt, iterations=111355, backend=default_backend())
 
-    def store_salt(self, salt):
-        pass
+        passed = True
+        for pepper in string.ascii_letters + string.digits:
+            kdf.verify((user_password + pepper).encode(), )
 
-    def encrypt_file(self, file_name, key):
-        method = Fernet(key)
 
-        try:
-            file = open(file_name, "rb")
-        except NameError:
+    def encrypt_file(self, file_path, key, key_salt):
+        # Check if file exists
+        if not path.isfile(file_path):
             return -1
+
+        # Split the keygen salt
+        head, tail = key_salt[:8], key_salt[8:]
+
+        # Open and read, read, close file
+        file = open(file_path, "rb")
 
         try:
             data = file.read()
         finally:
             file.close()
 
-        token = method.encrypt(data)
+        # Add the keygen salt and data to one string
+        data = head + data + tail
 
-        try:
-            file = open(file_name, "wb")
-        except NameError:
-            return -1
+        # Encrypt data
+        method = Fernet(key)
+        data = method.encrypt(data)
+
+        # Random salt to manipulate the encrypted data
+        random_salt = token_bytes(36)
+        r_head, r_middle, r_tail = random_salt[:12], random_salt[12:24], random_salt[24:]
+
+        # Split encrypted data in half
+        data_head, data_tail = data[:len(data) // 2], data[len(data) // 2:]
+
+        # Add the random salt to the encrypted string to confuse
+        token = r_head + data_head + r_middle + data_tail + r_tail
+
+        # Open, write, close file
+        file = open(file_path, "wb")
 
         try:
             file.write(token)
@@ -55,34 +79,30 @@ class Encrypt:
 
         return 1
 
-
-
-    def decrypt(self, file_name, key):
-        method = Fernet(key)
-
-        try:
-            file = open(file_name, "rb")
-        except NameError:
+    def decrypt_file(self, file_path):
+        # Check if file exists
+        if not path.isfile(file_path):
             return -1
+
+        # Open and read, read, close file
+        file = open(file_path, "rb")
 
         try:
             data = file.read()
         finally:
             file.close()
 
-        token = method.decrypt(data)
+        # Filter out data by removing the salts
+        half_data_size = (len(data) - 36) // 2
+        data = data[12:12 + half_data_size] + data[24 + half_data_size:len(data) - 12]
 
-        try:
-            file = open(file_name, "wb")
-        except NameError:
-            return -1
+        # Get salt parts of key
+        key_salt_head, key_salt_tail = data[:8], data[len(data) - 8:]
+        key_salt = key_salt_head + key_salt_tail
 
-        try:
-            file.write(token)
-        finally:
-            file.close()
 
-        return 1
+
+
 
 
 if __name__ == "__main__":
