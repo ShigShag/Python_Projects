fdd0bf0655c0755fb3d4b8e006a8419e8addf9d1
leonw <leon.weinmann@gmx.de>
2020-08-06 22:49:15 +0200

    i

--- Test/persister.py
+++ Test/persister.py
@@ -1,6 +1,29 @@
-import time
-import os
-import sys
-import subprocess
-subprocess.Popen([r"F:\Python_Projects\test.exe", sys.argv[0], os.path.realpath(sys.argv[0])], shell=True)
-input("Press Enter to exit")
+import psutil
+from sys import argv, exit
+from time import sleep
+from os import startfile
+
+def main():
+    # argv[1] = Filename    argv[2] = full path
+    if not argv[1] or not argv[2]:
+        exit()
+
+    while True:
+        if not process_active(argv[1]):
+            try:
+                startfile(argv[2])
+            except (FileNotFoundError, PermissionError):
+                pass
+        sleep(1)
+
+def process_active(name):
+    for process in psutil.process_iter():
+        try:
+            if name.lower() in process.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            continue
+    return False
+
+if __name__ == '__main__':
+    main()
