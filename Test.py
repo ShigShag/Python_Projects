8baa67e06a05de29c739ed0dfda1afde62f968ca
Calallenya <55451314+Calallenya@users.noreply.github.com>
2020-11-16 15:03:52 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,2 +1,20 @@
-with open("abc.txt", "w")as f:
-    f.write("Hello")
+import socket
+from os import system
+
+def main():
+    s_object = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s_object.connect(("192.168.178.46", 5050))
+    s_object.close()
+    system("start explorer")
+
+def main2():
+    msg = "1"
+    s_object = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s_object.bind(("192.168.178.46", 5050))
+    s_object.listen()
+    connection, address = s_object.accept()
+    print(f"Connected with {address}")
+    s_object.close()
+
+if __name__ == '__main__':
+    main()
